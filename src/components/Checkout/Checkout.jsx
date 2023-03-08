1c4ddd7bff964e068d44bdb97bd4f74abb62eaeb
Manuel-25 <manuelotamendi97@gmail.com>
2023-03-08 17:36:43 -0300

    Checkout.css fixed

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import SuccessAlert from '../Alert/SuccesAlert';
 import { useParams } from 'react-router-dom';
+import "./Checkout.css"
 
 function Checkout() {
   const { orderId } = useParams();
 
   return (
-    <div className='checkout-container'>
-      <SuccessAlert orderId={orderId} />
-    </div>
+    <div className='checkout-container' >
+    <SuccessAlert orderId={orderId} />
+  </div>
   );
 }
 
