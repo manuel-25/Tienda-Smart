cf7eaa1c5cefd477b3df9234d8eb192cefeafcbc
Manuel-25 <manuelotamendi97@gmail.com>
2023-03-13 21:23:58 -0300

    Sidebar added to navbar (works) needs to close one clicked on link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,46 +4,47 @@ import { Link, NavLink } from "react-router-dom"
 import { useRef } from 'react';
 
 function NavBar() {
-    //const hamburger = document.querySelector('.hamburger')
     const hamburgerRef = useRef(null);
+    const mobileNavRef = useRef(null);
 
     function handleHamburger() {
         hamburgerRef.current.classList.toggle('is-active');
+        mobileNavRef.current.classList.toggle('is-active')
+        document.body.classList.toggle('is-menu-open');
       }
 
 
     return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <h1>
+                        <Link to="/"><img className="logo" src={"/images/tiendaSmartLogo.png"} alt="Logo"></img></Link>
+                    </h1>
 
-        <nav className="navbar">
-            <div className="navbar-container">
-                <h1>
-                    <Link to="/"><img className="logo" src={"/images/tiendaSmartLogo.png"} alt="Logo"></img></Link>
-                </h1>
+                    <div className="menu"> 
+                        <NavLink to="/categoria/Smartphones" >Smartphones</NavLink>
+                        <NavLink to="/categoria/Tablets" className="Tablets " >Tablets</NavLink>
+                        <NavLink to="/categoria/Accesorios" className="Accesorios " >Accesorios</NavLink>
+                    </div>
 
-                <div className="menu"> 
-                    <NavLink to="/categoria/Smartphones" >Smartphones</NavLink>
-                    <NavLink to="/categoria/Tablets" className="Tablets " >Tablets</NavLink>
-                    <NavLink to="/categoria/Accesorios" className="Accesorios " >Accesorios</NavLink>
-                </div>
-
-                <CartWidget />
-
-                <button className="hamburger" onClick={handleHamburger} ref={hamburgerRef} >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-                
-                <nav className="mobile-navbar">
-                    <NavLink to="/categoria/Smartphones" >Smartphones</NavLink>
-                    <NavLink to="/categoria/Tablets" className="Tablets " >Tablets</NavLink>
-                    <NavLink to="/categoria/Accesorios" className="Accesorios " >Accesorios</NavLink>
                     <CartWidget />
-                </nav>
-            </div>
 
-            <script src="/navbar.js"></script>
+                    <button className="hamburger" onClick={handleHamburger} ref={hamburgerRef} >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    
+                </div>
+            </nav>
+            <nav className="sidebar" ref={mobileNavRef}>
+            <NavLink to="/categoria/Smartphones" >Smartphones</NavLink>
+            <NavLink to="/categoria/Tablets" className="Tablets " >Tablets</NavLink>
+            <NavLink to="/categoria/Accesorios" className="Accesorios " >Accesorios</NavLink>
+            <CartWidget />
         </nav>
+    </div>
 
     )
 }
